01b1dac532e196f16af14b90fcefd001572969c9
Himanshu <hs127010@gmail.com>
2023-07-28 01:50:20 +0530

    added posts, comments endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from api.models import Post, Comment
 
 class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True,read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'posts', 'comments']
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    comments = serializers.PrimaryKeyRelatedField(many=True,read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'owner', 'comments']
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'body', 'owner', 'post']
+
